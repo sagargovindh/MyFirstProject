21ab21526e5db6cb9bf00a42ea4baf42b2ad90c3
sagargovindh <163328467+sagargovindh@users.noreply.github.com>
2024-03-18 17:59:55 +0530

    Create Sample.java
    Frist Java Code

---
+++ Sample.java
@@ -0,0 +1,11 @@
+public class Sample{
+    public static void main(String args[]){
+      int a=10;
+      int b=20;
+      public static void display(){
+        System.out.println("Hii Everyone");
+      }
+      System.out.println(a);
+      System.out.println(b);
+    }
+  }
