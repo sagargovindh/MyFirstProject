8c734bc04513b03c625def1838b02a842148aeaa
sagargovindh <163328467+sagargovindh@users.noreply.github.com>
2024-03-24 12:55:21 +0530

    subtracted two variable

--- Sample.java
+++ Sample.java
@@ -2,7 +2,6 @@ public class Sample{
     public static void main(String args[]){
       int a=100;
       int b=200;
-      System.out.println(a);
-      System.out.println(b);
+      System.out.println(a-b);
     }
   }
