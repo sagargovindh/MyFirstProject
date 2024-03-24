28d2d9ddbd0bff6ba14f4e69cd10c969d420ab51
sagargovindh <211900343sagargovindh018@gmail.com>
2024-03-24 12:51:07 +0530

    removed print statement

--- Sample.java
+++ Sample.java
@@ -2,9 +2,6 @@ public class Sample{
     public static void main(String args[]){
       int a=100;
       int b=200;
-      public static void display(){
-        System.out.println("Hii Everyone");
-      }
       System.out.println(a);
       System.out.println(b);
     }
