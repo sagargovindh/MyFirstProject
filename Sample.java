f8d6ce6c649b221f87304fe3674a1afc155da64a
sagargovindh <211900343sagargovindh018@gmail.com>
2024-03-24 12:44:19 +0530

    newfeature

--- Sample.java
+++ Sample.java
@@ -2,9 +2,7 @@ public class Sample{
     public static void main(String args[]){
       int a=100;
       int b=200;
-      public static void display(){
-        System.out.println("Hii Everyone");
-      }
+      System.out.println("Hii Everyone");
       System.out.println(a);
       System.out.println(b);
     }
